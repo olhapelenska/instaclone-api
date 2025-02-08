cbe488a4baa542f3005a415742fb8644bcf1cb30
Olya <pelenska.olya@gmail.com>
2025-02-07 23:10:09 -0700

    small fixes

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,17 @@
-const connection =
-  process.env.JAWSDB_URL ||
-  (process.env.NODE_ENV === "development"
-    ? {
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        charset: "utf8",
-      }
-    : null);
+import "dotenv/config";
+
+const connection = process.env.JAWSDB_URL || {
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  charset: "utf8",
+};
+
+console.log(
+  "🚀 Database Connection:",
+  process.env.JAWSDB_URL ? "Using JawsDB" : "Using Local DB"
+);
 
 export default {
   client: "mysql2",
