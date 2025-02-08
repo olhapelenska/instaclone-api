0db0fcc882fa503c5daedd67fd936199b5468e0e
Olya <pelenska.olya@gmail.com>
2025-02-07 22:24:01 -0700

    add jawsdb

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,14 @@
 import "dotenv/config";
 
+const connection = process.env.JAWSDB_URL || {
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  charset: "utf8",
+};
+
 export default {
   client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    charset: "utf8",
-  },
+  connection,
 };
